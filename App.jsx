9fbc6388922c04204266dd52bee62f8efd087a24
Leeena92 <144833199+Leeena92@users.noreply.github.com>
2024-03-14 14:25:13 +0100

    Ke kontrole
    Znovu posílám

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/'
+import Hero from './Components/Hero/Hero'
+import Products from './Components/Products'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Products/>
+    </>
+  )
+}
+
+export default App
